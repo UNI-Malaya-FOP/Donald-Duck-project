6a38342f131dd618559a9c7556de08a13593bac8
M.Mahran <mmbmmahran@gmail.com>
2020-12-30 00:50:55 +0800

    first test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,17 @@
 package com.company;
 
+import edu.dataframe.DataFrame;
+import edu.dataframe.DataFrameException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataFrameException {
+        DataFrame dt = DataFrame.create()
+                                .addStringColumn("Name")
+                                .addIntegerColumn("Age")
+                                .addFloatColumn("Point")
+                                .append("Iris", 21, 99.9f)
+                                .append("Lily", 17, 73.7f);
+        dt.printDebug();
     }
 }
