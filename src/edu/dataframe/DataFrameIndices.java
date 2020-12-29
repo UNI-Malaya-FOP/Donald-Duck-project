970275b8a2ab17bf33702446c1200e9666431ed0
M.Mahran <mmbmmahran@gmail.com>
2020-12-29 20:51:10 +0800

    Add DataFrameIndices

---
+++ src/edu/dataframe/DataFrameIndices.java
@@ -0,0 +1,13 @@
+package src.edu.dataframe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataFrameIndices {
+
+    List<Integer> indices = new ArrayList<>();
+
+    public void add(Integer i) {
+        indices.add(i);
+    }
+}
