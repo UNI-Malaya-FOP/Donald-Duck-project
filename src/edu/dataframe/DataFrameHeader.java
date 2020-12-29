8fce60c4e151ca9d5353fee70b0683b1df6dcebc
M.Mahran <mmbmmahran@gmail.com>
2020-12-30 00:41:34 +0800

    Add method to get class from header

--- src/edu/dataframe/DataFrameHeader.java
+++ src/edu/dataframe/DataFrameHeader.java
@@ -2,17 +2,28 @@ package edu.dataframe;
 
 import edu.dataframe.column.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @SuppressWarnings("rawtypes")
 public class DataFrameHeader {
 
+    ArrayList<String> names = new ArrayList<>();
     HashMap<String, Class<? extends DataFrameColumn>> columnClassMap = new HashMap<>();
     HashMap<String, Class<? extends Comparable<?>>> typeClassMap = new HashMap<>();
 
     public void add(String name, Class<? extends DataFrameColumn> columnClass) {
+        names.add(name);
         columnClassMap.put(name, columnClass);
         typeClassMap.put(name, getTyeClass(columnClass));
+    }
+
+    public Class<? extends DataFrameColumn> getColumnClass (String name) {
+        return columnClassMap.get(name);
+    }
+
+    public Class<? extends DataFrameColumn> getColumnClass (Integer index) {
+        return columnClassMap.get(names.get(index));
     }
 
     private Class<? extends Comparable<?>> getTyeClass(Class<? extends DataFrameColumn> columnClass) {
@@ -23,5 +34,13 @@ public class DataFrameHeader {
         if (columnClass == StringColumn.class)
             return String.class;
         return null;
+    }
+
+    public Class<?> getTypeClass(String name) {
+        return typeClassMap.get(name);
+    }
+
+    public Class<? extends Comparable<?>> getTypeClass(Integer index) {
+        return typeClassMap.get(names.get(index));
     }
 }
