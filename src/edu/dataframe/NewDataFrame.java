3d948e67dc07fb70d07b4472bba54002414a538b
M.Mahran <mmbmmahran@gmail.com>
2020-12-29 21:04:19 +0800

    Add instace for NewDataFrame

--- src/edu/dataframe/NewDataFrame.java
+++ src/edu/dataframe/NewDataFrame.java
@@ -1,5 +1,10 @@
 package src.edu.dataframe;
 
+import java.util.HashMap;
+
 public class NewDataFrame implements DataFrame {
 
+    DataFrameHeader header = new DataFrameHeader();
+    DataFrameIndices indices = new DataFrameIndices();
+    HashMap<String, DataFrameColumn<?,?>> column = new HashMap<>();
 }
