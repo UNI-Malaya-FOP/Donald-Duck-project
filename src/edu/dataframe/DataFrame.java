7815127f8e1c2e210a3f069debff29d24cadbe0b
M.Mahran <mmbmmahran@gmail.com>
2020-12-30 00:00:24 +0800

    Add method for adding column

--- src/edu/dataframe/DataFrame.java
+++ src/edu/dataframe/DataFrame.java
@@ -1,7 +1,44 @@
-package src.edu.dataframe;
+package edu.dataframe;
 
 public interface DataFrame {
 
+    /**
+     * Add a column to DataFrame.
+     * @param column to add.
+     * @return DataFrame for method chain.
+     * @throws DataFrameException if column size does not match or if column name already exist.
+     */
+    DataFrame addColumn(DataFrameColumn<?> column) throws DataFrameException;
+
+    /**
+     * Add an empty Integer column.
+     * @param name of new Column.
+     * @return DataFrame for method chain.
+     * @throws DataFrameException if column name already exist.
+     */
+    DataFrame addIntegerColumn(String name) throws DataFrameException;
+
+    /**
+     * Add an empty Float column.
+     * @param name of new Column.
+     * @return DataFrame for method chain.
+     * @throws DataFrameException if column name already exist.
+     */
+    DataFrame addFloatColumn(String name) throws DataFrameException;
+
+    /**
+     * Add an empty String column.
+     * @param name of new Column.
+     * @return DataFrame for method chain.
+     * @throws DataFrameException if column name already exist.
+     */
+    DataFrame addStringColumn(String name) throws DataFrameException;
+
+
+    /**
+     * Create an empty new DataFrame.
+     * @return DataFrame for method chain.
+     */
     static DataFrame create() {
         return new NewDataFrame();
     }
--- src/edu/dataframe/NewDataFrame.java
+++ src/edu/dataframe/NewDataFrame.java
@@ -1,10 +1,49 @@
-package src.edu.dataframe;
+package edu.dataframe;
+
+import edu.dataframe.column.*;
 
 import java.util.HashMap;
 
 public class NewDataFrame implements DataFrame {
 
+    int rowNum = 0;
+    int columnNum = 0;
     DataFrameHeader header = new DataFrameHeader();
     DataFrameIndices indices = new DataFrameIndices();
-    HashMap<String, DataFrameColumn<?,?>> column = new HashMap<>();
+    HashMap<String, DataFrameColumn<?>> column = new HashMap<>();
+
+    @Override
+    public DataFrame addColumn(DataFrameColumn<?> column) throws DataFrameException {
+        if(column.size() != columnNum)
+            throw new DataFrameException("");
+        if(this.column.containsKey(column.getName()))
+            throw new DataFrameException("");
+        this.column.put(column.getName(), column);
+        header.add(column.getName(), column.getClass());
+        return this;
+    }
+
+    @Override
+    public DataFrame addIntegerColumn(String name) throws DataFrameException {
+        if(this.column.containsKey(name))
+            throw new DataFrameException("");
+        IntegerColumn column = new IntegerColumn(name);
+        return addColumn(column);
+    }
+
+    @Override
+    public DataFrame addFloatColumn(String name) throws DataFrameException {
+        if(this.column.containsKey(name))
+            throw new DataFrameException("");
+        FloatColumn column = new FloatColumn(name);
+        return addColumn(column);
+    }
+
+    @Override
+    public DataFrame addStringColumn(String name) throws DataFrameException {
+        if(this.column.containsKey(name))
+            throw new DataFrameException("");
+        StringColumn column = new StringColumn(name);
+        return addColumn(column);
+    }
 }
