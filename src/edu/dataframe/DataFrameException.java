d27bc97e86bef90b38996064930cf8097e21a0db
M.Mahran <mmbmmahran@gmail.com>
2020-12-30 00:00:44 +0800

    Add custom exception

---
+++ src/edu/dataframe/DataFrameException.java
@@ -0,0 +1,13 @@
+package edu.dataframe;
+
+public class DataFrameException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DataFrameException(String s) {
+        super(s);
+    }
+}
