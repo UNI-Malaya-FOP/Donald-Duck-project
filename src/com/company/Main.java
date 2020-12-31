56712272bafab64a5aaf2511d1490437475ff9a1
M.Mahran <mmbmmahran@gmail.com>
2020-12-31 21:34:16 +0800

    test sort, map and filter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
 import edu.dataframe.DataFrame;
+import edu.dataframe.DataFrameColumn;
 import edu.dataframe.DataFrameException;
+
+import java.util.Collections;
 
 public class Main {
 
@@ -10,8 +13,18 @@ public class Main {
                                 .addStringColumn("Name")
                                 .addIntegerColumn("Age")
                                 .addFloatColumn("Point")
-                                .append("Iris", 21, 99.9f)
-                                .append("Lily", 17, 73.7f);
-        dt.printDebug();
+                                .append("Lily", 17, 13.7f)
+                                .append(null, 24, 91.9f)
+                                .append("Iris", 21, 49.9f)
+                                .append("Earl", 24, 94.9f)
+                                .append("Earl", 27, 95.9f)
+                                .append("Lily", 17, 73.7f)
+                                .append("Lily", 14, 71.7f);
+        dt.dropNull("Name");
+        dt.sortBy("Name", Collections.reverseOrder());
+        dt.getFloatColumn("Point").doMap(n -> n + 100);
+        dt.getIntegerColumn("Age").doFilter(n -> n > 20);
+        dt.resetIndices();
+        dt.print();
     }
 }
