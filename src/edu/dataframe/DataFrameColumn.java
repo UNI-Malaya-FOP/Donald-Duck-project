58ba19533056a3db149f182ee016ac2d9fda046d
M.Mahran <mmbmmahran@gmail.com>
2020-12-31 21:30:56 +0800

    fix rename, add sort, map, filter

--- src/edu/dataframe/DataFrameColumn.java
+++ src/edu/dataframe/DataFrameColumn.java
@@ -1,24 +1,34 @@
 package edu.dataframe;
 
+import edu.dataframe.column.*;
+
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class DataFrameColumn<T extends Comparable<T>> implements Iterable<T>{
 
     private String name;
-    private final DataFrame dataFrame;
+    private NewDataFrame dataFrame;
     private final List<T> column = new ArrayList<>();
     private final DataFrameIndices indices = new DataFrameIndices();
 
     protected DataFrameColumn(String name, DataFrame dataFrame) {
-        this.dataFrame = dataFrame;
+        this.dataFrame = (NewDataFrame) dataFrame;
         this.name = name;
+    }
+
+    protected void reDataFrame(DataFrame dataFrame) {
+        this.dataFrame = (NewDataFrame) dataFrame;
     }
 
     public void rename(String name) throws DataFrameException {
         Set<String> names = dataFrame.getHeader().getNames();
-        if (!names.contains(name))
+        if (!dataFrame.getHeader().getNames().contains(name))
             this.name = name;
-        throw new DataFrameException("Name " + name + " already exist.");
+        else
+            throw new DataFrameException("Name " + name + " already exist.");
     }
 
     public String getName() {
@@ -104,6 +114,95 @@ public class DataFrameColumn<T extends Comparable<T>> implements Iterable<T>{
 
     public void replace(int index, T element) {
         column.set(index, element);
+    }
+
+    public void sort(Comparator<T> comparator) {
+        HashMap<T, List<Integer>> map = getTempMap();
+        column.sort(comparator);
+        List<Integer> howChange = getHowChange(map);
+        this.indices.sortBy(howChange);
+        dataFrame.getColumnChange(this, howChange);
+    }
+
+    private HashMap<T, List<Integer>> getTempMap() {
+        HashMap<T, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < column.size(); i++) {
+            if (!map.containsKey(column.get(i))) {
+                List<Integer> tmp = new ArrayList<>();
+                tmp.add(i);
+                map.put(column.get(i), tmp);
+            } else
+                map.get(column.get(i)).add(i);
+        }
+        return map;
+    }
+
+    private List<Integer> getHowChange(HashMap<T, List<Integer>> map) {
+        List<Integer> howChange = new ArrayList<>();
+        T curr = null;
+        for (T element : column) {
+            T prev = curr;
+            curr = element;
+            if (curr != prev)
+                howChange.addAll(map.get(curr));
+        }
+        return howChange;
+    }
+
+    protected void sortBy(List<Integer> howChange) {
+        List<T> newColumn = new ArrayList<>(column.size());
+        for (int i : howChange)
+            newColumn.add(column.get(i));
+        column.clear();
+        column.addAll(newColumn);
+        this.indices.sortBy(howChange);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <C extends DataFrameColumn<?>> C getSubColumn(String name) throws DataFrameException {
+        var columnClass = dataFrame.getHeader().getColumnClass(this.name);
+        if (columnClass == IntegerColumn.class) {
+            return (C) new IntegerColumn(name);
+        } else if (columnClass == FloatColumn.class) {
+            return (C) new FloatColumn(name);
+        } else if (columnClass == StringColumn.class) {
+            return (C) new StringColumn(name);
+        } else
+            throw new DataFrameException("This Column is not subclass of DataFrameColumn");
+    }
+
+    public DataFrameColumn<T> map(Function<T, T> function) throws DataFrameException {
+        List<T> mapped = column.stream().map(function).collect(Collectors.toList());
+        DataFrameColumn<T> col = getSubColumn("mapped" + name);
+        mapped.forEach(col::append);
+        return col;
+    }
+
+    public void doMap(Function<T,T> function) {
+        List<T> mapped = column.stream().map(function).collect(Collectors.toList());
+        column.clear();
+        column.addAll(mapped);
+    }
+
+    public DataFrameColumn<T> filter(Predicate<T> predicate) throws DataFrameException {
+        List<T> filtered = column.stream().filter(predicate).collect(Collectors.toList());
+        DataFrameColumn<T> col = getSubColumn("filtered" + name);
+        filtered.forEach(col::append);
+        return col;
+    }
+
+    public void doFilter(Predicate<T> predicate) throws DataFrameException {
+        List<T> filtered = column.stream().filter(predicate).collect(Collectors.toList());
+        HashMap<T, List<Integer>> map = getTempMap();
+        column.clear();
+        column.addAll(filtered);
+        List<Integer> howChange = getHowChange(map);
+        indices.sortBy(howChange);
+        dataFrame.getColumnChange(this, howChange);
+    }
+
+    protected void resetIndices() {
+        indices.resetIndices();
     }
 
     @Override
