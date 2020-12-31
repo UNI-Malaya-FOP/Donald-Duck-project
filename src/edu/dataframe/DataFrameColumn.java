e7a146592b2000b518768b90c46828f55aff4b56
M.Mahran <mmbmmahran@gmail.com>
2020-12-31 13:26:41 +0800

    Add method to get duplicate

--- src/edu/dataframe/DataFrameColumn.java
+++ src/edu/dataframe/DataFrameColumn.java
@@ -1,14 +1,12 @@
 package edu.dataframe;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class DataFrameColumn<T extends Comparable<T>> implements Iterable<T>{
 
-    String name;
-    List<T> column = new ArrayList<>();
-    DataFrameIndices indices = new DataFrameIndices();
+    private String name;
+    private final List<T> column = new ArrayList<>();
+    private final DataFrameIndices indices = new DataFrameIndices();
 
     protected DataFrameColumn(String name) {
         this.name = name;
@@ -33,6 +31,42 @@ public class DataFrameColumn<T extends Comparable<T>> implements Iterable<T>{
 
     public T get(int index) {
         return column.get(index);
+    }
+
+    private Set<T> getDuplicate() {
+        final Set<T> testAdd = new HashSet<>();
+        final Set<T> duplicate = new HashSet<>();
+        column.forEach(e -> {
+            if (!testAdd.add(e)) {
+                duplicate.add(e);
+            }
+        });
+        return duplicate;
+    }
+
+    public T getFirstDuplicate() {
+        Set<T> duplicate = getDuplicate();
+        for (T e : column) {
+            if (duplicate.contains(e))
+                return e;
+        }
+        return null;
+    }
+
+    public List<Integer> getFirstDuplicateIndices() {
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < column.size(); i++) {
+            T tmp = column.get(i);
+            for (int j = i + 1; j < column.size(); j++) {
+                if (tmp.equals(column.get(j)) && indices.size() == 0)
+                    indices.add(i);
+                if (tmp.equals(column.get(j)))
+                    indices.add(j);
+            }
+            if (indices.size() != 0)
+                return indices;
+        }
+        return null;
     }
 
     public int size() {
