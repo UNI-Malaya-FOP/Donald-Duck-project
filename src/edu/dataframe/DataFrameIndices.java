805b4945cb48aa7ab6fcc82f73cc7b66d54ca840
M.Mahran <mmbmmahran@gmail.com>
2020-12-31 21:33:34 +0800

    add method to workaround with sort

--- src/edu/dataframe/DataFrameIndices.java
+++ src/edu/dataframe/DataFrameIndices.java
@@ -8,6 +8,10 @@ public class DataFrameIndices {
     private int index = 1;
     private final List<Integer> indices = new ArrayList<>();
 
+    public int size() {
+        return indices.size();
+    }
+
     public void addIndex() {
         indices.add(index++);
     }
@@ -20,12 +24,28 @@ public class DataFrameIndices {
         indices.remove(index);
     }
 
+    public void clear() {
+        indices.clear();
+    }
+
+    Integer get(int index) {
+        return indices.get(index);
+    }
+
     public int indexOf(int index) {
         return indices.indexOf(index);
     }
 
     public boolean contains(int index) {
         return indices.contains(index);
+    }
+
+    void sortBy(List<Integer> howChange) {
+        List<Integer> newIndices = new ArrayList<>(indices.size());
+        for (int i : howChange)
+            newIndices.add(indices.get(i));
+        indices.clear();
+        indices.addAll(newIndices);
     }
 
     private void copyTo(List<Integer> indices) {
@@ -35,6 +55,15 @@ public class DataFrameIndices {
 
     public void copyTo(DataFrameColumn<?> column) {
         copyTo(column.getIndices().indices);
+    }
+
+    protected void resetIndices() {
+        this.index = 1;
+        int oldSize = indices.size();
+        indices.clear();
+        for (int i = 0; i < oldSize; i++) {
+            indices.add(index++);
+        }
     }
 
     @Override
