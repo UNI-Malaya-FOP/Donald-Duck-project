0b2230d471602ec2323d6eae450f2ab3ea988b93
faiqali1 <74228240+faiqali1@users.noreply.github.com>
2021-01-05 09:15:26 +0300

    Create Delete.java

---
+++ Delete.java
@@ -0,0 +1,30 @@
+public class Delete {
+    public void deletecolum (String [] list) {
+    int [] ref = new int[array.length];
+    for (String p: list) {
+
+
+        double[][] array;
+        for (int i = 0; i < array.length; i++) {
+             if (dataframe.header.equals(p) == 0) {
+              ref [i] = dataframe.indexof(p);
+             }
+        }
+    }
+    for (int i = 0; i < array.list; i++) {
+    // mathod to save only the column in in ref []
+    }
+
+
+
+
+}
+public void row_range (int a,int b){
+//    for (int i = 0; i < array[0].length; i++) {
+//
+//    }
+
+    }
+
+
+}
