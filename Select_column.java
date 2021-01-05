14e31e105752b321fc632869d1408c34ee01f898
faiqali1 <74228240+faiqali1@users.noreply.github.com>
2021-01-05 08:45:27 +0300

    Create Select_column.java
    Two select_row methods

---
+++ Select_column.java
@@ -0,0 +1,41 @@
+public void deletecolum (String [] list) {
+    int [] ref = new int[array.length];
+    for (String p: list) {
+
+
+        double[][] array;
+        for (int i = 0; i < array.length; i++) {
+             if (dataframe.header.equals(p) == 0) {
+              ref [i] = dataframe.indexof(p);
+             }
+        }
+    }
+    for (int i = 0; i < array.list; i++) {
+    // mathod to save only the column in in ref []
+    }
+
+
+
+
+}
+// method 2 
+   public void deletecolum (String [] list) {
+    int [] ref = new int[array.length];
+    for (String p: list) {
+
+
+        double[][] array;
+        for (int i = 0; i < array.length; i++) {
+             if (dataframe.header.equals(p) == 0) {
+              ref [i] = dataframe.indexof(p);
+             }
+        }
+    }
+    for (int i = 0; i < array.list; i++) {
+    // mathod to save only the column in in ref []
+    }
+
+
+
+    
+}
