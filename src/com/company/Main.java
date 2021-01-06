fe35a4ec54eba82b9ca6d42ef4ace94a4a0d828f
M.Mahran <mmbmmahran@gmail.com>
2021-01-06 14:10:33 +0800

    Test big file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,17 @@ public class Main {
 
     public static void main(String[] args) throws DataFrameException, IOException {
 
-        File file = new File("src\\resources\\test.csv");
-        URL url = new URL("https://raw.githubusercontent.com/UNI-Malaya-FOP/Donald-Duck-project/main/src/resources/Data.csv");
-
-        DataFrame dt = DataFrame.load(file);
+        File file1 = new File("src\\resources\\test.csv");
+        DataFrame dt = DataFrame.load(file1);
         dt.print();
+
+        URL url = new URL("https://raw.githubusercontent.com/UNI-Malaya-FOP/Donald-Duck-project/main/src/resources/Data.csv");
         DataFrame dt2 = DataFrame.load(url);
         dt2.print();
+
+        File file2 = new File("src\\resources\\CAvideos.csv");
+        DataFrame dt3 = DataFrame.load(file2);
+        dt3.print();
     }
 }
 
